1d836009f38f4185ab4aae4dcc08aa1b7c4390f3
Luiggi Goicochea <luire11@luiggis-MacBook-Pro.local>
2019-05-20 09:04:03 -0400

    upload files

---
+++ src/main/java/com/tts/TechTalentTwitter/model/Role.java
@@ -0,0 +1,40 @@
+package com.tts.TechTalentTwitter.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import lombok.NoArgsConstructor;
+
+@SpringBootApplication
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "role_id")
+	private Long id;
+	//adder getter and setter, testing
+
+	private String role;
+	
+	public String getRole() {
+		return role;
+		
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+}
